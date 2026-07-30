10d986865243e98a5e56e976185154d59d79b1c1
iamruhaan <ruhaanjoshi2006@gmail.com>
2026-07-30 20:12:05 +0530

    chore: remove binaries from repo, update gitignore

--- mobile/tunnel.js
+++
@@ -1,4 +0,0 @@
-const ngrok = require('@expo/ngrok');
-ngrok.connect({ addr: 3000, authtoken_from_env: false })
-  .then(url => console.log('NGROK_URL=' + url))
-  .catch(err => console.error(err));
